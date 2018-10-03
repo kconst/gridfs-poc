76de44dbb9f276bab57a847085e59131b1c649a0
Kyle Constance <kyle.constance@bamtechmedia.com>
2018-10-02 22:20:29 -0400

    adding comments and example file

--- createBucketAndUpload.js
+++ createBucketAndUpload.js
@@ -3,11 +3,14 @@ const fs = require('fs');
 const assert = require('assert');
 
 const client = new mongodb.MongoClient();
+// connect to our existing (and running) mongo server, use/create the testing db
 client.connect('mongodb://localhost:27017/testing', function(error, db) {
     assert.ifError(error);
 
+    // create/connect a mongodb gridfs bucket in the db
     var bucket = new mongodb.GridFSBucket(db);
 
+    // open a file for reading and pipe the contents of the file into mongodb
     fs.createReadStream('./dog.jpg').
     pipe(bucket.openUploadStream('dog.jpg')).
     on('error', function(error) {
--- retrieveFile.js
+++ retrieveFile.js
@@ -3,13 +3,18 @@ const fs = require('fs');
 const assert = require('assert');
 
 const client = new mongodb.MongoClient();
+// connect to the testing db in our running mongodb server
 client.connect('mongodb://localhost:27017/testing', function(error, db) {
     assert.ifError(error);
 
+    // create/connect a mongodb gridfs bucket in the db
     var bucket = new mongodb.GridFSBucket(db);
 
+    // open file for download stream
     bucket.openDownloadStreamByName('dog.jpg').
+    // start downloading at the first byte
     start(0).
+    // provide the filename to save as
     pipe(fs.createWriteStream('./output.jpg')).
     on('error', function(error) {
         assert.ifError(error);
